ea3e56f27500fd9690fe98773b306337423972d2
Ernesto Custodio <ernestojosecg@gmail.com>
2020-05-14 13:02:33 +0200

    Style

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,7 +4,7 @@ import { StyleCell } from './styles/StyleCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyleCell type={'L'} color={TETROMINOS['L'].color}>cell</StyleCell>
+    <StyleCell type={'L'} color={TETROMINOS['L'].color} />
 );
 
 export default Cell;
