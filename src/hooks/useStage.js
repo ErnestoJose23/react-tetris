5f7385bba229494b993882a028494e84bdc860d6
Ernesto Custodio <ernestojosecg@gmail.com>
2020-05-19 12:03:04 +0200

    Collision

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -6,25 +6,31 @@ export const useStage = (player, resetPlayer) => {
 
     useEffect(() => {
         const updateStage = prevStage => {
-            const newStage = prevStage.map(row => 
+            const newStage = prevStage.map(row =>
                 row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
             );
 
             player.tetromino.forEach((row, y) => {
                 row.forEach((value, x) => {
-                    if( value !== 0){
+                    if (value !== 0) {
                         newStage[y + player.pos.y][x + player.pos.x] = [
                             value,
-                            `${player.collided ? 'merged': 'clear'}`,
+                            `${player.collided ? 'merged' : 'clear'}`,
                         ]
                     }
                 });
             });
+
+            //Check if we collided
+            if (player.collided) {
+                resetPlayer();
+            }
+
             return newStage;
         };
 
-        setStage(prev =>updateStage(prev))
-    }, [player]);
+        setStage(prev => updateStage(prev))
+    }, [player, resetPlayer]);
 
     return [stage, setStage];
 }
