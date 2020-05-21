5cc6a51476ea9130ae22e323b950b3315e62bdc7
Ernesto Custodio <ernestojosecg@gmail.com>
2020-05-21 11:45:48 +0200

    Clear rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -14,8 +14,11 @@ export const useStage = (player, resetPlayer) => {
                 if (row.findIndex(cell => cell[0] === 0) === -1) {
                     setRowsCleared(prev => prev + 1);
                     ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return ack;
                 }
-            })
+                ack.push(row);
+                return ack;
+            }, [])
         const updateStage = prevStage => {
             const newStage = prevStage.map(row =>
                 row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
